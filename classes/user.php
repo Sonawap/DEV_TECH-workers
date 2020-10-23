60f24c3b13731473df9ec9e996e5b29b4d6ee9d9
Sonawap <paulsola79@gmail.com>
2020-10-23 13:34:28 -0700

    Fixed

--- classes/user.php
+++ classes/user.php
@@ -10,31 +10,41 @@ class User
 
 	public $password = ['son', 'lan', 'kay', 'ayb', 'top'];
 
+	/////// return array list of users
 	public function getUsernames(){
 		return $this->username;
 	}
 
+
+	///// return array list length
 	public function getUsernamesLength(){
 		return count($this->username);
 	}
 
-	public function showUsernames(){
+	public function usernameAndPassword(){
+
+
+		////// display array of Names
+		echo "<h1>Usernames of some workers at DEV_TECH</h1>";
 		for($x = 0; $x < $this->getUsernamesLength(); $x++) {
 		    echo "<ul><li>".$this->getUsernames()[$x] ." </li></ul>";
 		}
-	}
+	
+		// ////// Checking if any of the name is less than is 6 letters or Greater than 8 letters
 
-	public function wrongUsernames(){
+		echo "<h1>List of usernames that are less than 6 letters or Greater than 8 letters</h1>";
 		for($x = 0; $x < $this->getUsernamesLength(); $x++) {
 
-			//////// get only name that is less than is letters
+			//////// get only name that is less than 6 is letters or Greater than 8
 			if (strlen($this->getUsernames()[$x]) < 6 || strlen($this->getUsernames()[$x]) > 8 ) {
 				echo "<b>".$this->getUsernames()[$x]. "</b> is Less Than 6 letters or Greater than 8 letters [".strlen($this->getUsernames()[$x])." Letters]<br>";
 			}
 		}
-	}
 
-	public function changeUsernames(){
+		// ////////////// Changing username of names less than 6 letters or Greater than 8 letters
+
+		echo "<h1>Assigning a default username to usernames less than 6 letters or Greater than 8 letters</h1>";
+
 		for($x = 0; $x < $this->getUsernamesLength(); $x++) {
 
 			//////// Assigning a default username to usernames less than 6 letters
@@ -44,9 +54,8 @@ class User
 			}
 		}
 
-	}
-
-	public function newList(){
+		// ///////////// Listing new usernames
+		echo "<h1>List of usernames of the workers now with Generated Pasword</h1>";
 		for($x = 0; $x < $this->getUsernamesLength(); $x++) {
 		    if (strlen($this->getUsernames()[$x]) < 6 || strlen($this->getUsernames()[$x]) > 8) {
 				echo "<ul><li>".$this->getUsernames()[$x] = "user".rand(1000, 9999);
@@ -55,8 +64,8 @@ class User
 				echo "<ul><li>".$this->getUsernames()[$x] ." => " . $this->password[$x].rand(100,999). $this->password[$x] ."</li></ul>";
 			}
 		}
-
 	}
+
 }
 
 $user = new User();
